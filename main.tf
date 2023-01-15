44e48bbb0227d9481241bf552fab93c466677553
b-cor <54155861+b-cor@users.noreply.github.com>
2023-01-14 22:30:15 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,6 @@
 provider "aws" {
   region = "us-west-2"
-}
-
-module "s3backend" {
-  source    = "github.com/b-cor/terraform-aws-s3bucket-backend"
-  namespace = "team-rocket"
-}
- 
-output "s3backend_config" {
-  value = module.s3backend.config
+  profile = "tf-user"
 }
 
 data "aws_region" "current" {}
